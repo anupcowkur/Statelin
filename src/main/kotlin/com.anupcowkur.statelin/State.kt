97bb8dbf0d8b5e239ae5aa48423600dc10216fb6
Veyndan Stuart <veyndan@gmail.com>
2018-02-06 15:21:40 +0000

    Fix duplicate trigger comparison (#2)
    With the current implementation, exceptions aren't always thrown if a duplicate trigger handler is added. This is because the states are being compared by reference instead of by the contents of the object. By overriding the .equals(Any?) method of State this problem is now fixed. Note that onEnter and onExit aren't used in the comparison as these shouldn't be considered (hence the use of manually overriding .equals(Any?) and hashCode() compared to using a data class).

--- src/main/kotlin/com.anupcowkur.statelin/State.kt
+++ src/main/kotlin/com.anupcowkur.statelin/State.kt
@@ -1,4 +1,8 @@
 package com.anupcowkur.statelin
 
 class State(private val name: String, internal val onEnter: (() -> Unit)? = null, internal val onExit: (() -> Unit)? = null) {
+
+    override fun hashCode() = name.hashCode()
+
+    override fun equals(other: Any?) = other is State && other.name == name
 }
